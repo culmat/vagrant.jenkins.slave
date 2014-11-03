f5fce576d155cfb8f663fbfbd3bbb01eb3f502da
Matthias Cullmann <matthias.cullmann@gmail.com>
2014-11-03 21:36:43 +0100

    using vagrant provisioner instead of upstart

--- etc/bootstrap.sh
+++ etc/bootstrap.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
 apt-get update
 sudo apt-get install default-jre
-sudo apt-get install git
-cp /vagrant/etc/mounted-vagrant.conf /etc/init/
-/vagrant/bin/startUp.sh
+sudo apt-get install git -y
